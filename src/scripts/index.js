e6e2f8659893492f450eb2a269629e3193c43198
Cameron Zaas <cameronzaas@gmail.com>
2017-08-17 20:12:56 -0700

    initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+import '../styles/index.scss';
+
+import { 
+  app,
+  h,
+} from 'hyperapp';
+
+
+
+
+app({
+  root: document.getElementById('mount'),
+
+  state: {
+
+  },
+
+  actions: {
+
+  },
+
+  view: (state, actions) => (
+    <main class="container">
+      <h1>CZaas</h1>
+    </main>
+  ),
+});
