f0a6dd536c32824641a6e51e4fa69f0ce4f19664
Cameron Zaas <cameronzaas@gmail.com>
2017-08-19 10:06:31 -0700

    Moved multi graph image into stylesheets for repeat in the event more pricing groups can/will be added

--- src/scripts/components/Card.js
+++ src/scripts/components/Card.js
@@ -6,46 +6,6 @@ import {
   commaPlacer,
 } from './../util/commaPlacer.js';
 
-const PricingOptionTable = (props) => {
-  var currentOptions = props.pricingOptions.filter((eachOption) => eachOption.ProgramID === parseInt(props.ProgramID, 10));
-
-  /*
-  Animation calculation happening here:
-  CSS: transition: height; overflow: hidden;
-
-  - If visible, multiplies the amount of current options based on the height of each one
-  - Else not visible height of component is zero. 
-  */
-  var currentHeight = { height: (props.isVisible) ? `${(currentOptions.length + 2) * 20}px` : '0' };
-
-  return (
-    <div class="card__bottom-sales__table-wrapper" style={currentHeight}>
-      <table class={`card__bottom-sales__table`}>
-        <tr>
-          <th>Price Name</th>
-          <th>Current</th>
-          <th>1 - Year</th>
-        </tr>
-        <tr>
-          <td colspan="2">
-            {currentOptions.map((eachOption) => (
-              <tr>
-                <td>{eachOption.Name}</td>
-                <td>${commaPlacer(eachOption.Sales)}</td>
-              </tr>
-            ))}
-          </td>
-          <td>
-            <img src={'/assets/spark_lines.png'} />
-          </td>
-        </tr>
-      </table>
-    </div>
-  );
-};
-
-
-
 /*
 # Card
 
@@ -62,7 +22,7 @@ const Card = (props) => (
     <img class="card__graph" src={'/assets/graph.png'} />
 
     <section class="card__current-sales">
-      <table class="card__current-sales__table">
+      <table class="card__current-sales__table" cellspacing="0" cellpadding="0">
         <tbody>
           <tr>
             <th>Total Monthly</th>
@@ -87,5 +47,45 @@ const Card = (props) => (
     </section>
   </div>
 );
+
+
+
+function PricingOptionTable(props) {
+  var currentOptions = props.pricingOptions.filter((eachOption) => eachOption.ProgramID === parseInt(props.ProgramID, 10));
+
+  /*
+  Animation calculation happening here:
+  CSS: transition: height; overflow: hidden;
+
+  - If visible, multiplies the amount of current options based on the height of each one (the + 1 is to account for the static headers)
+  - Else not visible height of component is zero. 
+  */
+  var currentHeight = { height: (props.isVisible) ? `${(currentOptions.length + 1) * 20}px` : '0' };
+
+  return (
+    <div class="card__bottom-sales__table-wrapper" style={currentHeight}>
+      <table class={`card__bottom-sales__table`} cellspacing="0" cellpadding="0">
+        <tr>
+          <th>Price Name</th>
+          <th>Current</th>
+          <th>1 - Year</th>
+        </tr>
+        <tr>
+          <td colspan="2">
+            {currentOptions.map((eachOption) => (
+              <tr>
+                <td>{eachOption.Name}</td>
+                <td>${commaPlacer(eachOption.Sales)}</td>
+              </tr>
+            ))}
+          </td>
+          <td class="card__bottom-sales__table__multi-graph">
+            
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
+};
 
 export default Card;
